9ff593ded6d59e49b1f04c2a6a8eb6de684f7ea1
phamchithien2112 <127403788+phamchithien2112@users.noreply.github.com>
2024-07-18 21:43:54 +0700

    .
    bookshop

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.z4xjb5d.mongodb.net',
+  DB_USER: 'Shopping',
+  DB_PASS: '123',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'phamchithien2112@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'rcobkiuegvaxwrfa',
+  JWT_SECRET: '12345678',
+  JWT_EXPIRES: '65656443534453222222222224667778896', // in milliseconds
+};
+module.exports = MyConstants;
